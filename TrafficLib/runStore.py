e1d0c3eb6cce6304460fcb5c0e526f8e4e9ad49c
PrinceNgattaiLam <dprince@styx.ia.unc.edu>
2017-06-30 11:10:34 -0400

    ENH: Fix memory usage issues

--- TrafficLib/runStore.py
+++ TrafficLib/runStore.py
@@ -2,22 +2,19 @@ import numpy as np
 import argparse
 import os
 import tensorflow as tf
-import sys
-sys.path.append("/root/work/TRAFFIC/Traffic/TrafficLib/")
 from storeDef import *
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_string('train_dir', '',    ' training input directory')
-flags.DEFINE_string('test_dir', '',    ' training input directory')
-flags.DEFINE_string('valid_dir', '',    ' training input directory')
-flags.DEFINE_string('original_dir', '/root/work/Multiclass/Test/',    ' Oiginal input Test directory before pre processing')
+flags.DEFINE_string('train_dir', '',    ' training directory')
+flags.DEFINE_string('test_dir', '',    ' testing directory')
+flags.DEFINE_string('valid_dir', '',    ' validation directory')
+flags.DEFINE_string('original_dir', '',    ' Oiginal input Test directory before pre processing')
 flags.DEFINE_integer('num_landmarks',  5,      'Number of landmarks')
 flags.DEFINE_integer('num_points',  50,      'Number of points on each fiber')
 flags.DEFINE_boolean('landmarks', True, 'Compute landmarks features')
 flags.DEFINE_boolean('curvature', True, 'Compute curvature features')
 flags.DEFINE_boolean('torsion', True, 'Compute torsions features')
-
 
 def read_training(train_dir, num_landmarks, num_points, landmarks, curvature, torsion):
     dataset = []
@@ -26,7 +23,8 @@ def read_training(train_dir, num_landmarks, num_points, landmarks, curvature, to
         os.path.join(train_dir, d) for d in sorted(os.listdir(train_dir))
         if os.path.isdir(os.path.join(train_dir, d))]
     for label, folder in enumerate(folders):
-        fiber_files = [os.path.join(folder, d) for d in sorted(os.listdir(folder))]
+        fiber_files = [
+            os.path.join(folder, d) for d in sorted(os.listdir(folder))]
         for fiber in fiber_files:
             fiber_file = os.path.join(folder, fiber)
             try:
@@ -36,25 +34,103 @@ def read_training(train_dir, num_landmarks, num_points, landmarks, curvature, to
                 labels += data_label
             except IOError as e:
                 print('Could not read:', fiber_file, ':', e, '- it\'s ok, skipping.')
-    # num_fib = len(dataset)
-    # num_feat = len(dataset[0])
-    # num_pt = len(dataset[0][0])
-    # print "Data Set : ",num_fib, num_feat, num_pt
+    num_fib = len(dataset)
+    num_feat = len(dataset[0])
+    num_pt = len(dataset[0][0])
+
     # Shuffle data
-    # print "Dataset: ", dataset
-    # permutation = np.random.permutation(num_fib)
-    # dataset = np.array(dataset) #[permutation]
-    # labels = np.array(labels) #[permutation]
+    permutation = np.random.permutation(num_fib)
+    # dataset = np.asrray(dataset)[permutation]
+    # labels = np.asrray(labels)[permutation]
 
     # Create an instance of the class train_data where we are going to store our training data
-    # train_data = data_set(num_fib, num_feat, num_pt)
-    # train_data.data = dataset[0:num_fib]
-    # train_data.labels = labels[0:num_fib]
-    # print "Data Set :", train_data.data.shape
+    train_data = data_set(num_fib, num_feat, num_pt)
+    data_t = train_data.data
+    print ""
+    print np.asarray(labels)[permutation[50]]
+    print ""
+    print np.asarray(dataset)[permutation[50]]
+    # print ""
+    # print np.asarray(labels)[permutation[50]]
+    # print ""
+    # print np.asarray(dataset)[permutation[50]]
 
-    return dataset, labels
+    for i in xrange(num_fib):
+        data_t[i] = dataset[permutation[i]]
+        dataset[permutation[i]] = []
+    del dataset
+    train_data.labels=np.asarray(labels)[permutation]
+    del labels
+    # train_data.data = np.asarray(dataset)[permutation]
+    # del dataset
+    # train_data.labels = np.asarray(labels)[permutation]
+    # print train_data.labels[50]
+    # print ""
+    # print train_data.data[50]
+    # for i in xrange(250):
+    #     print train_data.labels[i]
+    
+    print "Data Set :", train_data.data.shape
+
+    return train_data
 
 
+# def read_testing(test_dir, src_dir, num_landmarks, num_points, landmarks, curvature, torsion):
+#     dataset = []
+#     data_names = []
+#     fibers = [
+#         os.path.join(test_dir, d) for d in sorted(os.listdir(test_dir))
+#         if os.path.isfile(os.path.join(test_dir, d))]
+
+#     for fiber_file in fibers:
+#         if fiber_file.rfind(".vtk")!= -1 or fiber_file.rfind(".vtp")!= -1 :
+#             name_fiber = os.path.basename(fiber_file)
+#             name_src = os.path.join(src_dir, name_fiber)
+#             try:
+#                 data_fiber, data_name = fiber_extract_feature(fiber_file, landmarks, curvature, torsion,
+#                                                       num_landmarks, num_points, name_src, train=False)
+#                 dataset += data_fiber
+#                 data_names += data_name
+#             except IOError as e:
+#                 print('Could not read:', fiber_file, ':', e, '- it\'s ok, skipping.')
+#         else:
+#             print 'Could not read:', fiber_file, 'Not a vtk file ...Skipping.'
+
+#     num_fib = len(dataset)
+#     num_feat = len(dataset[0])
+#     num_pt = len(dataset[0][0])
+#     print "Nb Fibers:", num_fib
+#     print "Nb Features:", num_feat
+#     print "Nb Points:", num_pt
+
+#     # # Create an instance of the class train_data where we are going to store our training data
+#     # test_data = data_set(num_fib, num_feat, num_pt) # type_lab=np.str, type_dat=np.float64)
+#     # data_t = test_data.data
+#     # print ""
+#     # print np.asarray(data_names)[50]
+#     # print ""
+#     # print np.asarray(dataset)[50]
+
+#     # for i in xrange(num_fib):
+#     #     data_t[i] = dataset[0]
+#     #     del dataset[0]
+#     # test_data.labels=np.asarray(data_names)
+#     # del data_names
+
+#     # print test_data.labels[50]
+#     # print ""
+#     # print test_data.data[50]
+#     # print ""
+#     # print "Testing Set :", test_data.data.shape
+#     # return test_data
+    
+#     # Create an instance of the class train_data where we are going to store our training data
+#     test_data = data_set(num_fib, num_feat, num_pt) # type_lab=np.str, type_dat=np.float64)
+#     test_data.data = np.array(dataset)
+#     test_data.labels = np.array(data_names)
+#     print "Testing Set :", test_data.data.shape
+#     return test_data
+#     
 def read_testing(test_dir, src_dir, num_landmarks, num_points, landmarks, curvature, torsion):
     dataset = []
     data_names = []
@@ -74,46 +150,49 @@ def read_testing(test_dir, src_dir, num_landmarks, num_points, landmarks, curvat
             except IOError as e:
                 print('Could not read:', fiber_file, ':', e, '- it\'s ok, skipping.')
         else:
-            print('Could not read:', fiber_file, 'Not a vtk file ...Skipping.')
+            print 'Could not read:', fiber_file, 'Not a vtk file ...Skipping.'
 
-    # num_fib = len(dataset)
-    # num_feat = len(dataset[0])
-    # num_pt = len(dataset[0][0])
-    # print "Nb Fibers:", num_fib
-    # print "Nb Features:", num_feat
-    # print "Nb Points:", num_pt
+    num_fib = len(dataset)
+    num_feat = len(dataset[0])
+    num_pt = len(dataset[0][0])
+    print "Nb Fibers:", num_fib
+    print "Nb Features:", num_feat
+    print "Nb Points:", num_pt
 
     # Create an instance of the class train_data where we are going to store our training data
-    # test_data = data_set(num_fib, num_feat, num_pt) # type_lab=np.str, type_dat=np.float64)
-    # test_data.data = np.array(dataset)
-    # test_data.labels = np.array(data_names)
-    # print "Testing Set :", test_data.data.shape
-    return dataset, data_names
-
-
-def run_store(train_dir='', test_dir='', valid_dir='', original_dir='', num_landmarks=5, num_points=50, lmOn=True, curvOn=True, torsOn=True):
+    test_data = data_set(num_fib, num_feat, num_pt) # type_lab=np.str, type_dat=np.float64)
+    data_t = test_data.data
+    for i in xrange(num_fib):
+        data_t[i] = dataset[0]
+        del dataset[0]
+    test_data.labels=np.asarray(data_names)
+    del data_names
+    print "Testing Set :", test_data.data.shape
+    return test_data
+    
+def run_store(train_dir='', valid_dir='', test_dir='', original_dir='', num_landmarks=5, num_points=50, lmOn=True, curvOn=True, torsOn=True):
     if train_dir:
-        dataset, labels = read_training(train_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
+        train_set = read_training(train_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
 
         #   Convert to Examples and write the result to TFRecords.
-        convert_to(dataset, labels, 'train', train_dir)
+        convert_to(train_set, 'train', train_dir)
 
     elif valid_dir:
-        dataset, labels = read_training(valid_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
+        valid_set = read_training(valid_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
 
         #   Convert to Examples and write the result to TFRecords.
-        convert_to(dataset, labels, 'valid', valid_dir)
+        convert_to(valid_set, 'valid', valid_dir)
 
     elif test_dir:
-        dataset, labels = read_testing(test_dir, original_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
+        test_set = read_testing(test_dir, original_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
 
         # Convert to Examples and write the result to TFRecords.
-        convert_to(dataset, labels, 'test', test_dir)
+        convert_to(test_set, 'test', test_dir)
         # Convert_to(test_set.validation, 'validation')
 
 
 def main(_):
-    run_store(FLAGS.train_dir, FLAGS.test_dir, FLAGS.valid_dir, FLAGS.original_dir, FLAGS.num_landmarks, FLAGS.num_points, FLAGS.landmarks, FLAGS.curvature, FLAGS.torsion)
+    run_store(FLAGS.train_dir, FLAGS.valid_dir, FLAGS.test_dir,  FLAGS.original_dir, FLAGS.num_landmarks, FLAGS.num_points, FLAGS.landmarks, FLAGS.curvature, FLAGS.torsion)
   # Get the data.
 
 
