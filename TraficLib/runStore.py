11910d4a3d2e4cc1ffb8dde8f2baf51609829cc6
Adrien Boucaud <adrienboucaud.cpe+git@gmail.com>
2018-02-21 12:37:01 -0500

    ENH: Added SMOTE usage with flag, updated read functions

--- TraficLib/runStore.py
+++ TraficLib/runStore.py
@@ -1,7 +1,9 @@
+
 import numpy as np
 import argparse
 import os
 import tensorflow as tf
+import smote
 from storeDef import *
 
 flags = tf.app.flags
@@ -15,6 +17,7 @@ flags.DEFINE_integer('num_points',  50,      'Number of points on each fiber')
 flags.DEFINE_boolean('landmarks', True, 'Compute landmarks features')
 flags.DEFINE_boolean('curvature', True, 'Compute curvature features')
 flags.DEFINE_boolean('torsion', True, 'Compute torsions features')
+flags.DEFINE_boolean('use_smote', False, 'Use synthetic oversampling')
 
 def read_training(train_dir, num_landmarks, num_points, landmarks, curvature, torsion):
     dataset = []
@@ -30,55 +33,14 @@ def read_training(train_dir, num_landmarks, num_points, landmarks, curvature, to
             try:
                 data_fiber, data_label = fiber_extract_feature(fiber_file, landmarks, curvature, torsion,
                                                       num_landmarks, num_points, label, train=True)
-                dataset += data_fiber
-                labels += data_label
+                if len(data_fiber) > 0:
+                  dataset.extend(data_fiber.reshape((len(data_fiber),-1)))
+                  labels.extend(data_label)
             except IOError as e:
                 print('Could not read:', fiber_file, ':', e, '- it\'s ok, skipping.')
-    num_fib = len(dataset)
-    num_feat = 0
-    if landmarks:
-        num_feat+=num_landmarks
-    if curvature:
-        num_feat+=1
-    if torsion:
-        num_feat+=1
 
-    # Shuffle data
-    permutation = np.random.permutation(num_fib)
-    # dataset = np.asrray(dataset)[permutation]
-    # labels = np.asrray(labels)[permutation]
-
-    # Create an instance of the class train_data where we are going to store our training data
-    train_data = data_set(num_fib, num_feat, num_points)
-    data_t = train_data.data
-    # print ""
-    # print np.asarray(labels)[permutation[50]]
-    # print ""
-    # print np.asarray(dataset)[permutation[50]]
-    # print ""
-    # print np.asarray(labels)[permutation[50]]
-    # print ""
-    # print np.asarray(dataset)[permutation[50]]
-
-    for i in xrange(num_fib):
-        data_t[i] = dataset[permutation[i]]
-        dataset[permutation[i]] = []
-    del dataset
-    train_data.labels=np.asarray(labels)[permutation]
-    del labels
-    # train_data.data = np.asarray(dataset)[permutation]
-    # del dataset
-    # train_data.labels = np.asarray(labels)[permutation]
-    # print train_data.labels[50]
-    # print ""
-    # print train_data.data[50]
-    # for i in xrange(50):
-    #     print train_data.labels[i]
-    #     sys.stdout.flush()
-    
-    print "Data Set :", train_data.data.shape
-
-    return train_data
+    permutation = np.random.permutation(len(dataset))
+    return np.array(dataset)[permutation], np.array(labels)[permutation]
 
 def read_testing(test_dir, src_dir, num_landmarks, num_points, landmarks, curvature, torsion):
     dataset = []
@@ -94,35 +56,29 @@ def read_testing(test_dir, src_dir, num_landmarks, num_points, landmarks, curvat
             try:
                 data_fiber, data_name = fiber_extract_feature(fiber_file, landmarks, curvature, torsion,
                                                       num_landmarks, num_points, name_src, train=False)
-                dataset += data_fiber
-                data_names += data_name
+                dataset.extend(data_fiber.reshape((len(data_fiber),-1)))
+                data_names.extend(data_name)
             except IOError as e:
                 print('Could not read:', fiber_file, ':', e, '- it\'s ok, skipping.')
         else:
             print 'Could not read:', fiber_file, 'Not a vtk file ...Skipping.'
-
-    num_fib = len(dataset)
-    num_feat = len(dataset[0])
-    num_pt = len(dataset[0][0])
-    print "Nb Fibers:", num_fib
-    print "Nb Features:", num_feat
-    print "Nb Points:", num_pt
-
-    # Create an instance of the class train_data where we are going to store our training data
-    test_data = data_set(num_fib, num_feat, num_pt) # type_lab=np.str, type_dat=np.float64)
-    data_t = test_data.data
-    for i in xrange(num_fib):
-        data_t[i] = dataset[0]
-        del dataset[0]
-    test_data.labels=np.asarray(data_names)
-    del data_names
-    print "Testing Set :", test_data.data.shape
-    return test_data
     
-def run_store(train_dir='', valid_dir='', test_dir='', original_dir='', num_landmarks=5, num_points=50, lmOn=True, curvOn=True, torsOn=True):
+    return np.array(dataset), np.array(data_names)
+    
+def run_store(train_dir='', valid_dir='', test_dir='', original_dir='', num_landmarks=5, num_points=50, lmOn=True, curvOn=True, torsOn=True, use_smote=False):
     if train_dir:
-        train_set = read_training(train_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
-
+        dataset, labels = read_training(train_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
+        if use_smote:
+            dataset, labels = smote.generate_with_SMOTE(dataset.reshape(len(dataset),-1),labels)
+            permutation = np.random.permutation(len(dataset))
+            dataset = dataset[permutation]
+            labels = labels[permutation]
+            print('Smote success !')
+        dataset = dataset.reshape(len(dataset), num_landmarks + 2, num_points)
+    	train_set = data_set(len(dataset), num_landmarks + 2, num_points)
+    	train_set.data = dataset
+    	train_set.labels = labels
+        print ('Final dataset shape: ', np.shape(train_set.data))
         #   Convert to Examples and write the result to TFRecords.
         convert_to(train_set, 'train', train_dir)
 
@@ -133,15 +89,19 @@ def run_store(train_dir='', valid_dir='', test_dir='', original_dir='', num_land
         convert_to(valid_set, 'valid', valid_dir)
 
     elif test_dir:
-        test_set = read_testing(test_dir, original_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
-
+        data, names = read_testing(test_dir, original_dir, num_landmarks, num_points, lmOn, curvOn, torsOn)
+        data = data.reshape(len(data), num_landmarks + 2, num_points)
+        print (np.shape(data))
+        test_set = data_set(len(data), num_landmarks + 2, num_points)
+        test_set.data = data
+        test_set.labels = names 
         # Convert to Examples and write the result to TFRecords.
         convert_to(test_set, 'test', test_dir)
         # Convert_to(test_set.validation, 'validation')
 
 
 def main(_):
-    run_store(FLAGS.train_dir, FLAGS.valid_dir, FLAGS.test_dir,  FLAGS.original_dir, FLAGS.num_landmarks, FLAGS.num_points, FLAGS.landmarks, FLAGS.curvature, FLAGS.torsion)
+    run_store(FLAGS.train_dir, FLAGS.valid_dir, FLAGS.test_dir,  FLAGS.original_dir, FLAGS.num_landmarks, FLAGS.num_points, FLAGS.landmarks, FLAGS.curvature, FLAGS.torsion, FLAGS.use_smote)
   # Get the data.
 
 
