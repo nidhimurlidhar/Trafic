2d116c04e5ad66c79cf0e6005c7e22030dcb5e2a
Juan Carlos Prieto <juanprietob@gmail.com>
2018-10-22 13:05:47 -0400

    ENH: Add install rules for Trafic
    Create package configuration to use find_package. Add different targets

--- Trafic.cmake
+++ Trafic.cmake
@@ -21,8 +21,29 @@ if(${LOCAL_PROJECT_NAME}_BUILD_SLICER_EXTENSION)
 	include(${Slicer_USE_FILE})
 endif()
 
+SETIFEMPTY(INSTALL_RUNTIME_DESTINATION bin)
+SETIFEMPTY(INSTALL_LIBRARY_DESTINATION bin)
+SETIFEMPTY(INSTALL_ARCHIVE_DESTINATION lib/static)
+SETIFEMPTY(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
+SETIFEMPTY(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/lib)
+SETIFEMPTY(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin)
+
 find_package(niral_utilities REQUIRED
 	HINTS ${niral_utilities_DIR})
+
+foreach(niral_utilities_lib ${niral_utilities_LIBRARIES})
+
+    get_target_property(niral_utilities_location ${niral_utilities_lib} LOCATION_RELEASE)
+    if(NOT EXISTS ${niral_utilities_location})
+      message(STATUS "skipping niral_utilities_lib install rule: [${niral_utilities_location}] does not exist")
+      continue()
+    endif()
+
+    install(PROGRAMS ${niral_utilities_location} 
+    	DESTINATION ${INSTALL_RUNTIME_DESTINATION}
+    	COMPONENT RUNTIME)
+    
+  endforeach()
 
 #-----------------------------------------------------------------------------
 # Extension modules
@@ -36,3 +57,49 @@ if(${LOCAL_PROJECT_NAME}_BUILD_SLICER_EXTENSION)
 	include(${Slicer_EXTENSION_GENERATE_CONFIG})
 	include(${Slicer_EXTENSION_CPACK})
 endif()
+
+### This is the config file for niral_utilities
+# Create the FooBarConfig.cmake and FooBarConfigVersion files
+
+if(WIN32 AND NOT CYGWIN)
+  set(DEF_INSTALL_CMAKE_DIR CMake)
+else()
+  set(DEF_INSTALL_CMAKE_DIR lib/CMake/Trafic)
+endif()
+set(INSTALL_CMAKE_DIR ${DEF_INSTALL_CMAKE_DIR} CACHE PATH
+  "Installation directory for CMake files")
+set(INSTALL_INCLUDE_DIR include)
+
+# Make relative paths absolute (needed later on)
+foreach(p LIB BIN INCLUDE CMAKE)
+  set(var INSTALL_${p}_DIR)
+  if(NOT IS_ABSOLUTE "${${var}}")
+    set(${var} "${CMAKE_INSTALL_PREFIX}/${${var}}")
+  endif()
+endforeach()
+
+file(RELATIVE_PATH REL_INCLUDE_DIR ${INSTALL_CMAKE_DIR} ${INSTALL_INCLUDE_DIR})
+# ... for the build tree
+set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+get_property(CONF_LIBRARIES GLOBAL PROPERTY Trafic_LIBRARIES)
+
+configure_file(TraficConfig.cmake.in
+  "${PROJECT_BINARY_DIR}/TraficConfig.cmake" @ONLY)
+# ... for the install tree
+set(CONF_INCLUDE_DIRS "\${Trafic_CMAKE_DIR}/${REL_INCLUDE_DIR}")
+configure_file(TraficConfig.cmake.in
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/TraficConfig.cmake" @ONLY)
+
+install(FILES
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/TraficConfig.cmake"
+  DESTINATION "${INSTALL_CMAKE_DIR}" COMPONENT dev)
+ 
+# Install the export set for use with the install-tree
+install(EXPORT TraficTargets DESTINATION
+"${INSTALL_CMAKE_DIR}" COMPONENT dev)
+
+export(TARGETS ${CONF_LIBRARIES}
+  FILE "${PROJECT_BINARY_DIR}/TraficTargets.cmake"
+  )
+
+
